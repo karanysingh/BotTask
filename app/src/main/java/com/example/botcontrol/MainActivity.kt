6d16d3c7c87684d866cddb5289dc8cb5651c9440
Karan Yuvraj Singh <karan124d@gmail.com>
2020-07-25 11:26:39 +0530

    Bluetooth Bot control

--- app/src/main/java/com/example/botcontrol/MainActivity.kt
+++ app/src/main/java/com/example/botcontrol/MainActivity.kt
@@ -1,41 +1,112 @@
 package com.example.botcontrol
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.util.*
+import kotlin.collections.ArrayList
+
+//import androidx.test.core.app.ApplicationProvider.getApplicationContext
+
 
 const val REQUEST_ENABLE_BT = 4
-class MainActivity : AppCompatActivity() {
+private const val TAG = "BOT CONTROL"
+const val MESSAGE_READ: Int = 0
+const val MESSAGE_WRITE: Int = 1
+const val MESSAGE_TOAST: Int = 2
+
+//val btHandler = Handler(Looper looper)
+open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //question  mark represents that its nullable
-        print("Aagya")
+
+        val scrollData: ArrayList<String> = arrayListOf("Data\n")
+        val dataBox: TextView = findViewById(R.id.textView3)
         val toggleBt: Button = findViewById(R.id.toggle)
         val toggleTransmission: Button = findViewById(R.id.transmission)
         val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-        toggleBt.setOnClickListener{
-            if (bluetoothAdapter == null){
-                print("Device doesn't support Bluetooth or no device found")
+        var deviceName: String? = null
+        var deviceHardwareAddress: String? = null
+        val connectBtn: Button = findViewById(R.id.connect)
+//        val btStream: MyBluetoothService = MyBluetoothService()
+        toggleBt.setOnClickListener {
+            if (bluetoothAdapter == null) {
+                println("Device doesn't support Bluetooth or no device found")
             }
-            if(bluetoothAdapter?.isEnabled == false) {
+            if (bluetoothAdapter?.isEnabled == false) {
+                val intent = Intent(this, bluetooth::class.java)
+                startActivity(intent)
                 val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-                }
-            if(bluetoothAdapter?.isEnabled == true){
+            }
+            if (bluetoothAdapter?.isEnabled == true) {
                 bluetoothAdapter.disable();
             }
         };
+        connectBtn.setOnClickListener {
+            if (bluetoothAdapter?.isEnabled == false) {
+                val toast: Toast = Toast.makeText(this, "Enable Bt first!", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+            if (bluetoothAdapter?.isEnabled == true) {
+                val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+                pairedDevices?.forEach { device ->
+                    deviceName = device.name
+                    deviceHardwareAddress = device.address // MAC address
+                }
+//            val uuid = UUID.fromString(deviceHardwareAddress)
+                scrollData.add(pairedDevices.toString())
+                dataBox.text = scrollData.toString()
+                println("These are paired already $deviceHardwareAddress")
+                val device = pairedDevices!!.first()
+                BluetoothClient(device).start()
+            }
+        }
         toggleTransmission.setOnClickListener {
             println(toggleTransmission.text)
-            if(toggleTransmission.text == "Manual!"){
+            if (toggleTransmission.text == "Manual!") {
                 toggleTransmission.setText(R.string.ToggleTransmissionAutomatic)
-            }
-            else{
+            } else {
                 toggleTransmission.setText(R.string.ToggleTransmissionManual)
             }
+        }
+//        bluetoothAdapter?.startDiscovery()
+    }
+
+}
+
+class BluetoothClient(device: BluetoothDevice) : Thread() {
+    val uuid: UUID = UUID.fromString("8989063a-c9af-463a-b3f1-f21d9b2b827b")
+    private val socket = device.createRfcommSocketToServiceRecord(uuid)
+    override fun run() {
+        Log.i("Client", "Connecting")
+        this.socket.connect()
+
+        Log.i("Client", "Sending")
+        val outputStream = this.socket.outputStream
+        val inputStream = this.socket.inputStream
+        try {
+            var message = "Hi"
+            outputStream.write(message.toByteArray())
+            outputStream.flush()
+            Log.i("Client", "Sent")
+
+        } catch (e: Exception) {
+            Log.e("client", "cannot send", e)
+        } finally {
+            outputStream.close()
+            inputStream.close()
+            this.socket.close()
         }
     }
 }
---
+++ app/src/main/java/com/example/botcontrol/bluetooth.kt
@@ -0,0 +1,110 @@
+package com.example.botcontrol
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.AdapterView
+var listDevices: ArrayList<String> = arrayListOf()
+val EXTRA_ADDRESS: String? = null
+open class bluetooth : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth)
+        val listDevicesView: ListView = findViewById<ListView>(R.id.bluetooth_listView)
+//        val redColor = Color.parseColor("#FF0000")
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+//        listDevicesView.setBackgroundColor(redColor)
+        val scanBtn = findViewById<Button>(R.id.scanBtn)
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        pairedDevicesList(pairedDevices,listDevicesView)
+        listDevicesView.adapter = MyCustomAdapter(this, listDevices)
+    }
+
+    private fun pairedDevicesList(pairedDevices: Set<BluetoothDevice>?,listDevicesView: ListView) {
+        if (pairedDevices != null) {
+            if (pairedDevices.isNotEmpty()) {
+                for (i in pairedDevices) {
+                    listDevices.add("${i.name} ${i.address}")
+                }
+            }
+        } else {
+            Toast.makeText(this, "No paired Device found", Toast.LENGTH_SHORT).show()
+        }
+//        val arrayAdapter: ArrayAdapter<String> =
+//            ArrayAdapter(this, android.R.layout.simple_list_item_1, listDevices)
+//        listDevicesView.adapter = arrayAdapter
+        listDevicesView.setOnItemClickListener { parent:AdapterView<*>, v:View, position:Int, id:Long ->
+//             Get the device MAC address, the last 17 chars in the View
+            if(position == 0){
+                Toast.makeText(this,"Clicked on first item",Toast.LENGTH_SHORT).show()
+            }
+            val info = (v as TextView).text.toString()
+            val address = info.substring(info.length - 17)
+//             Make an intent to start next activity.
+            val i = Intent(this, MainActivity::class.java)
+            println("Selected -------> $info $address")
+//            Change the activity.
+            i.putExtra(
+                EXTRA_ADDRESS,
+                address
+            )
+            finish()
+            startActivity(i)
+        }
+//    private val myListClickListener = { parent, v, position, id ->
+//            run {
+//                // Get the device MAC address, the last 17 chars in the View
+//                val info = (v as TextView).text.toString()
+//                val address = info.substring(info.length - 17)
+//                // Make an intent to start next activity.
+//                val i = Intent(this, MainActivity::class.java)
+//                //Change the activity.
+//                i.putExtra(
+//                    EXTRA_ADDRESS,
+//                    address
+//                )
+//                startActivity(i)
+//            }
+//        }
+
+    }}
+private class MyCustomAdapter(context: Context, items: ArrayList<String>) : BaseAdapter() {
+    private val mContext: Context
+    private val items: ArrayList<String>
+
+    init {
+        this.mContext = context
+        this.items = items
+    }
+
+    override fun getCount(): Int {
+//        TODO("Not yet Implemented")
+        return items.size
+    }
+
+    override fun getItem(position: Int): Any {
+//        TODO("Not yet implemented")
+        return "TEST STRING"
+    }
+
+    override fun getItemId(position: Int): Long {
+//        TODO("Not yet implemented")
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+//        TODO("Not yet implemented")
+        val textView = TextView(mContext)
+        textView.text = items[position]
+//        textView.text = "items[positi
+        return textView
+    }
+}
